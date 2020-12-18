a7353e0fa212b17f4a362d1c5c400bac14c5c3b9
PeachyDew <73276183+PeachyDew@users.noreply.github.com>
2020-12-18 12:17:26 +0500

    Base

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -8,6 +8,67 @@
 #include <windows.h> 
 using namespace std;
 
+class Base
+{
+public:
+	int valuebase;
+	Base() {//конструктор
+		cout << "Вызов конструктора Base" << endl;
+	}
+	Base(Base* obj) {//конструктор, принимающий аргумент и преобразущий его к типу конструктора
+		cout << "Вызов конструктора преобразования *Base" << endl;
+	}
+	Base(Base& obj) {//obj хранит адрес при передачи объекта по значению в качестве аргумента
+		cout << "Вызов копирующего конструктора &Base" << endl;
+	}
+	virtual ~Base() {
+		cout << "Виртуальный ~Base" << endl;
+	}
+	virtual string classname()
+	{
+		return "Базовый виртуальный метод string classname(), Base";
+	}
+	virtual bool isA(string classnm)//Базовый виртуальный метод string classname(), Base
+	{
+		if (classname() == classnm) {//проверка на принадлежность
+			cout << "true" << endl;
+			return true;
+		}
+		else {
+			cout << "false" << endl;
+			return false;
+		}
+	}
+	void method1_1() {
+		cout << "Метод method1_1 класса Base. Вызовется method 2_1: " << endl;
+		method2_1();
+	}
+	void method1_2() {
+		cout << "Метод method1_2 класса Base. Вызовется method 2_2:" << endl;
+		method2_2();
+	}
+	void method2_1() {
+		cout << "Метод method2_1 класса Base" << endl;
+	}
+	virtual void method2_2() {//виртуальный метод
+		cout << "Виртуальный метод method2_2 класса Base" << endl;
+	}
+	void method3() {
+		cout << "Метод method3 класса Base" << endl;
+	}
+	virtual void method4() {//виртуальный метод
+		cout << "Виртуальный метод method4 класса Base" << endl;
+	}
+	virtual void redefinition(int d) {//виртуальный метод переопределения переменной класса
+		valuebase = d;
+		cout << "Виртуальный метод redefinition у Base " << endl;
+	}
+	//virtual ~Base() // примечание: Деструктор виртуальный
+	//{
+	//	cout << "Виртуальный деструктор Base" << endl;
+
+	//}
+};
 
 
 int main() {
